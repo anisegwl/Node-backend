cc0cbab8c084bc4c3f3b485b15fff42cac1c48bc
anisegwl <anisgnawali34@gmail.com>
2025-05-28 21:18:39 +0545

    Routes

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
-const express = require('express');
+const express = require("express");
 const dotenv = require("dotenv");
-const connectDB = require('./db');
-
+const connectDB = require("./db");
+const { chats } = require("./data/data");
 const app = express();
 
-// Load env variables
 dotenv.config();
-
-// Connect to MongoDB
 connectDB();
+const port = process.env.PORT;
 
-// Middleware (optional, if you want to parse JSON in requests)
-app.use(express.json());
-
-// Routes
-app.get('/', (req, res) => {
-    res.send('Hello Hero!');
+app.get("/", (req, res) => {
+  res.send("Hello chiatra class ");
+});
+app.get("/about", (req, res) => {
+  res.send("This is about page");
+});
+app.get("/contact/address", (req, res) => {
+  res.send("i am from kathmandu");
+});
+app.get("/chats", (req, res) => {
+  res.send({ chats });
+});
+app.get("/chats/:id", (req, res) => {
+  // console.log(req.params.id);
+  const singleChat = chats.find((chat) => chat._id === req.params.id);
+  res.send({ singleChat });
 });
 
-app.get('/about', (req, res) => {
-    res.send('This is About Pagee!');
-});
+app.use("/api/auth", require("../backend/routes/Auth"));
 
-app.get('/contact-us/address', (req, res) => {
-    res.send('Kathmandu');
-});
-
-app.get('/testimonial', (req, res) => {
-    res.send('This is Testimonial!');
-});
-
-// Handle unknown routes
-app.use((req, res) => {
-    res.status(404).send('Page not found');
-});
-
-// Start server
-const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
---
+++ model/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  //instance create
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: false,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
